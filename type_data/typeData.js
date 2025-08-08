7779cddb36c745d9fb90514aa2f9928a165b7fbf
kimikerya <abidintamvan1@gmail.com>
2025-08-08 18:46:33 +0700

    javaScript Pembelajaran

---
+++ type_data/typeData.js
@@ -0,0 +1,7 @@
+// Di JS ada 3 type data yaitu var let const
+// var adalah type data yang bisa diubah" isi datanya
+// let sama kaya var tapi beberapa studi kasus let lebih diutungkan seperti penerapan scope global/lokal
+// const adalah type data yang tidak bisa di ubah dan di gugat dengan data baru
+var data = "Kamu"
+let iya = 135125
+const cihuy = 12415
