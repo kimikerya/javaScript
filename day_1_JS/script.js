d911e551f5565f44ccf016cdb36739b3bc0b34c9
kimikerya <abidintamvan1@gmail.com>
2025-08-09 22:43:29 +0700

    javaScript Pembelajaran

---
+++ day_1_JS/script.js
@@ -0,0 +1,16 @@
+// Jum'at 8/8/25
+
+// Di JS ada 3 type data yaitu var let const
+// var adalah type data yang bisa diubah" isi datanya
+// let sama kaya var tapi beberapa studi kasus let lebih diutungkan seperti penerapan scope global/lokal
+// const adalah type data yang tidak bisa di ubah dan di gugat dengan data baru
+var data = "Kamu"
+let iya = 135125
+const cihuy = 12415
+
+// Prompt adalah pop up yang bisa input data
+// alert adalah sama" pop up tapi, tidak input data, bisa menampilkan data di pop up hasil dari prompt
+//  console.log adalah mengetes data / menampilkan pesan atau nilai variabel ke konsol peramban
+
+let usiaKamu = prompt("Tuliskan nama kamu ");
+alert("Nama kamu adalah :" + usiaKamu);
--- external_javaScript_alert_promt_consoleLog/alert_promt_consoleLog.js
+++
@@ -1,6 +0,0 @@
-// Prompt adalah pop up yang bisa input data
-// alert adalah sama" pop up tapi, tidak input data, bisa menampilkan data di pop up hasil dari prompt
-//  console.log adalah mengetes data / menampilkan pesan atau nilai variabel ke konsol peramban
-
-let usiaKamu = prompt("Tuliskan nama kamu ");
-alert("Nama kamu adalah :" + usiaKamu);
