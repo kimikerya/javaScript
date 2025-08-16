25ae40c54a014a06218e98c29683430a8594a654
kimikerya <abidintamvan1@gmail.com>
2025-08-16 22:01:53 +0700

    javaScript Pembelajaran

--- video_pembelajaran_dea_afrizal/day_4_JS/script.js
+++ video_pembelajaran_dea_afrizal/day_4_JS/script.js
@@ -1,3 +1,14 @@
 // Selasa 12/08/25
+// Baru 10 menit
 
 // DOM
+// Di dalam dom ada document dulu dan isinya seperti
+document.body();
+
+// Jika ingin menambahkan sesuatu di halaman bodynya kurleb
+const iniBagianBody = document.body;
+
+// Ada banyak cara untuk memasukkan suatu kode untuk beberapa body seperti
+// append, textcontent, innertext
+const menambahkanH1 = document.createElement('h1');
+menambahkanH1.textContent = 'gw di h1'
